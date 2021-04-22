29353f58e2f71c874e2eeb4909c08d985fbc8eba
MandiYang <yangmandi2008@gmail.com>
2021-04-22 15:32:11 +0200

    Edited

--- MYgames_package/MYgames/guess_animal.py
+++ MYgames_package/MYgames/guess_animal.py
@@ -6,19 +6,21 @@ def control(guess, answer):
     else:
         print("Wrong answer, the answer is " + answer)
 score = 0
-print('Guess animal!')
 
-guess1 = input('Which animal has 4 legs and has a very long throat? : ')
-control(guess1, 'giraff')
-guess2 = input('What is the most fastest animal on land?:  ')
-control(guess2, 'cheetah')
-guess3 = input('What is the biggest animal in the world?:  ')
-control(guess3, 'blue whale')
-guess3 = input('What is the name of the smartest animal?:  ')
-control(guess3, 'human')
+if __name__=='__main__':
+    print('Guess the animal!')
+    
+    guess1 = input('Which animal has 4 legs and has a very long throat? : ')
+    control(guess1, 'giraff')
+    guess2 = input('What is the most fastest animal on land?:  ')
+    control(guess2, 'cheetah')
+    guess3 = input('What is the biggest animal in the world?:  ')
+    control(guess3, 'blue whale')
+    guess3 = input('What is the name of the smartest animal?:  ')
+    control(guess3, 'human')
 
-print('Your total score is ' + str(score))
+    print('Your total score is ' + str(score))
 
-if score>2:
-    print('Good job! You are a genius :)')
+    if score>2:
+        print('Good job! You are a genius :)')
         
